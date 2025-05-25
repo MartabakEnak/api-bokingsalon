473f6b9629e0f8789ae8565c327319bc5334971b
Mangwah87 <Mangwah87>
2025-05-25 10:58:52 +0800

    login

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -19,7 +19,81 @@
     <h1>about us</h1>
   </div>
 
+<div class="image-wrapper">
+    <img src="{{ asset('assets/img/fotopegawai.png') }}" alt="Tim Salon" class="tim-image">
+    <div class="image-overlay"></div>
+</div>
 
+<div class="container section-hero">
+  <div class="row align-items-center">
+    <!-- KIRI -->
+    <div class="col-md-6">
+      <h2 class="hero-title mb-3">Tiga Dekade Pelayanan Kecantikan Terpercaya</h2>
+      <p class="hero-text">
+        Salon Asih berdiri sejak tahun 1991, berlokasi di Jln. Pulau Bungin No.113, Denpasar Selatan. Sejak awal, Salon Asih hadir dengan satu tujuan memberikan pelayanan kecantikan terbaik yang mengutamakan kenyamanan, profesionalitas, dan hasil yang memuaskan. Dengan pengalaman lebih dari tiga dekade, Salon Asih telah menjadi pilihan terpercaya bagi para wanita Bali dan sekitarnya yang ingin tampil cantik dan percaya diri. Kami terus berkembang mengikuti tren dan teknologi terkini demi menghadirkan perawatan yang aman, modern, dan berkualitas tinggi.
+      </p>
+    </div>
+
+    <!-- KANAN -->
+    <div class="col-md-6">
+      <div class="hero-image-box w-100"></div>
+    </div>
+  </div>
+</div>
+
+<div class="container section-services">
+  <h2 class="section-title">Beyond Ordinary Beauty Services</h2>
+
+  <!-- Kartu 1 -->
+  <div class="service-card-1">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+        <img src="{{ asset('assets/img/img1.png') }}" alt="Service Image 1">
+      </div>
+      <div class="col-md-6 service-content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Blandit at maecenas dui sed amet sit enim vitae. Amet purus dictum urna sagittis dignissim. At fermentum iaculis ullamcorper orci.
+        </p>
+        <p>
+          Pellentesque id tempor lacus aliquet tempus vitae nibh habitasse consectetur. Augue quis cras blandit habitant neque. Faucibus vestibulum in nisl ligula ultrices et vehicula.
+        </p>
+        <button class="service-button">Konsultasi</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Kartu 2 (gambar kanan) -->
+  <div class="service-wrapper">
+  <div class="service-card-right position-relative">
+    <div class="service-content-right">
+      <p> Lorem ipsum dolor sit amet consectetur. Blandit at maecenas dui sed amet sit enim vitae. Amet purus dictum urna sagittis dignissim. At fermentum iaculis ullamcorper orci.</p>
+      <p>Pellentesque id tempor lacus aliquet tempus vitae nibh habitasse consectetur. Augue quis cras blandit habitant neque. Faucibus vestibulum in nisl ligula ultrices et vehicula.</p>
+      <button class="service-button">Konsultasi</button>
+    </div>
+    <img src="{{ asset('assets/img/img2.png') }}" alt="Adat" class="image-inside-card-right">
+  </div>
+</div>
+
+
+  <!-- Kartu 3 -->
+  <div class="service-card">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+        <img src="{{ asset('assets/img/img3.png') }}" alt="Service Image 3">
+      </div>
+      <div class="col-md-6 service-content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Blandit at maecenas dui sed amet sit enim vitae. Amet purus dictum urna sagittis dignissim. At fermentum iaculis ullamcorper orci.
+        </p>
+        <p>
+          Pellentesque id tempor lacus aliquet tempus vitae nibh habitasse consectetur. Augue quis cras blandit habitant neque. Faucibus vestibulum in nisl ligula ultrices et vehicula.
+        </p>
+        <button class="service-button">Konsultasi</button>
+      </div>
+    </div>
+  </div>
+
+</div>
     @include('partials.footer')
 </body>
 </html>
