807792f8611cb99754c903524adcaa585a0e04dd
Juventuss <134345238+MartabakEnak@users.noreply.github.com>
2025-07-02 22:27:31 +0800

    a

--- resources/views/contacUs.blade.php
+++ resources/views/contacUs.blade.php
@@ -150,11 +150,11 @@
 </div>
 
 
-
+@include('partials.footer')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
 
-    @include('partials.footer')
+
 </body>
 </html>
